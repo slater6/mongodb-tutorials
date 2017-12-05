d1566304968d5570bbbc46a03fe4a6c7e37e48f7
slater6 <slaters@gmail.com>
2017-12-04 22:51:12 -0500

    Added reading and saving mocha tests

--- udemy/tdd/test/test_helper.js
+++ udemy/tdd/test/test_helper.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo.dev:27017/users_test', { useMongoClient: true })
+mongoose.Promise = global.Promise
 
-mongoose
-    .connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', (error) => {
-        console.warn('Error',error)
+before((done) => {
+    mongoose.connect('mongodb://mongo.dev:27017/users_test', { useMongoClient: true })
+    mongoose
+        .connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('Error',error)
+        })
+})
+
+after(function() {
+    mongoose.disconnect();
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done()
     })
+})
