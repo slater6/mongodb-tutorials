a6951301ecd769fb5dae42a4dfe939d1c1399173
slater6 <slaters@gmail.com>
2017-12-17 21:51:41 -0500

    Setup Pagination

--- udemy/tdd/test/reading_test.js
+++ udemy/tdd/test/reading_test.js
@@ -2,11 +2,17 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users from db", () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(done => {
+    alex = new User({ name: "Alex" });
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()]).then(() =>
+      done()
+    );
   });
 
   it("finds all users with name Joe", done => {
@@ -25,5 +31,18 @@ describe("Reading users from db", () => {
       assert(user.name === joe.name);
       done();
     });
+  });
+
+  it("can skip and limit the result set", done => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
